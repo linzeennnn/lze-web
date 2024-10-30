44a43824c837abb7fd7a84c08d88144074f6145d
linzeennnn <linzeen2745276404@gmail.com>
2024-10-30 08:18:02 +0800

    incomplete

--- opt/lze-web/web/home/widget.js
+++ opt/lze-web/web/home/widget.js
@@ -1,4 +1,8 @@
 function allwidget(type){
+    let doc = [1, 2, 3].map(i => document.getElementById(`doc-li${i}`));
+    let not = [1, 2, 3].map(i => document.getElementById(`not-li${i}`));
+    let mon = [1, 2, 3].map(i => document.getElementById(`mon-li${i}`));
+    let pic=document.getElementById(`pic`);
 fetch(`${protocol}//${ip}/code/home/widget.php`, {
     method: 'POST',
     headers: {
@@ -10,6 +14,10 @@ fetch(`${protocol}//${ip}/code/home/widget.php`, {
     return response.json(); 
 })
 .then(data => {
+    doc.forEach((el, index) => el.innerText = data[`doc${index + 1}`]);
+    not.forEach((el, index) => el.innerText = data[`not${index + 1}`]);
+    mon.forEach((el, index) => el.innerText = data[`mon${index + 1}`]);
+    pic.style.backgroundImage = `url(../../icon/${data.pic1})`;
     console.log(data);
     console.log(data.doc1,data.doc2,data.doc3);
     console.log(data.not1,data.not2,data.not3);
