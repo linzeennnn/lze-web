b268fe59abccfe60eafdf1c0c6fa770c25661bc8
linzeennnn <linzeen2745276404@gmail.com>
2024-11-02 00:52:22 +0800

    rebuild bookmark

--- opt/lze-web/web/bok/bok.js
+++ opt/lze-web/web/bok/bok.js
@@ -43,8 +43,6 @@ function optionbar(status){
   }
 function handleScroll() {
     var scrollTop = window.scrollY || document.documentElement.scrollTop;
-        
-        // 当垂直滚动超过100px时改变背景颜色
         if (scrollTop > 100) {
             document.getElementById('top-btn').style.bottom = '7%';
           document.getElementById('top-bar').style.top = '10px';
@@ -72,8 +70,6 @@ function handleScroll() {
 window.addEventListener('scroll', handleScroll);
      function handleScroll() {
     var scrollTop = window.scrollY || document.documentElement.scrollTop;
-        
-        // 当垂直滚动超过100px时改变背景颜色
         if (scrollTop > 100) {
             document.getElementById('top-btn').style.bottom = '15%';
           document.getElementById('top-bar').style.top = '10px';
@@ -98,7 +94,8 @@ window.addEventListener('scroll', handleScroll);
 }
 window.addEventListener('scroll', handleScroll);
      function comin(){
-        showNotes();
+        pageloading(1);
+        getbok();
         theme(mode,"red")
         document.getElementById('top-bar').style.top ='100px';
         document.getElementById('option-bar').style.left='0';
@@ -114,7 +111,7 @@ function totop() {
     window.scroll({
       top: 0,
       left: 0,
-      behavior: 'smooth' // 添加平滑滚动效果
+      behavior: 'smooth' 
     });
   }
 function goBack() {
@@ -128,234 +125,114 @@ function goBack() {
         const text = await response.text();
         return text;
     }
-
-    async function getNoteFiles() {
-        const response = await fetch(`${protocol}//${ip}/code/Bookmark/bok_getFiles.php`,fetchtoken());
-        const noteFiles = await response.json();
-        xmlnologin(response);
-        return noteFiles;
-    }
-    async function showNotes() {
-            const notesContainer = document.getElementById('notes');
-
-            try {
-                const noteFiles = await getNoteFiles();
-
-                for (const file of Object.values(noteFiles)) {
-                    const linkname = file.replace(/\.[^/.]+$/, '');
-                    const filePath = `${protocol}//${ip}/file/Bookmark/${file}`;
-                    const content = await readFile(filePath);
-                    const noteElement = document.createElement('div');
-                    noteElement.className = 'note-element';
-                    noteElement.innerHTML = `
-                        <h3>${linkname}</h3>
-                        <div class="delete-btn" onclick="deleteNote('${file}')"></div>
-                        <div class="open-page-btn" >
-                            </div>
-                    `;
-
-                    noteElement.addEventListener('click', async function(event) {
-                        const target = event.target;
-                        let iframeSrc;
-                        if (file.endsWith('.bok')) {
-                            iframeSrc = content.startsWith('http') ? content : `${protocol}//${content}`;
-                        }
-                       else{
-                         iframeSrc = content.startsWith('http') ? content : `${filePath}`;
-                       }     
-                        if (target.classList.contains('delete-btn') || target.classList.contains('open-page-btn') || target.classList.contains('iframe-delete-btn')) {
-                            return;
-                        }
-
-                        const iframeContainer = noteElement.querySelector('.iframe-container');
-                        if (!iframeContainer) {
-                            openPage(iframeSrc);
-                            
-                            
-                        }
-                    });
-
-                    noteElement.querySelector('.open-page-btn').addEventListener('click', function() {
-                        createIframe(noteElement, content, filePath, file);
-                        
-                    });
-                    noteElement.querySelector('.open-page-btn').addEventListener('click', function() {
-                    this.style.display = 'none'; // 隐藏当前书签的打开网页按钮
-                });
-
-                    notesContainer.appendChild(noteElement);
-                }
-            } catch (error) {
-                console.error('Error reading files:', error);
-            }
-        }
-        function openPage(url) {
-            window.location.href = url;
-        }
-   
-
-   
-
-        function createIframe(container, content,filePath ,file) {
-
-            const iframeContainer = document.createElement('div');
-            iframeContainer.className = 'iframe-container';
-
-            let iframeSrc;
-                        if (file.endsWith('.bok')) {
-                            iframeSrc = content.startsWith('http') ? content : `${protocol}//${content}`;
-                        }
-                       else{
-                         iframeSrc = content.startsWith('http') ? content : `${filePath}`;
-                       }    
-            iframeContainer.innerHTML = `
-                <iframe src="${iframeSrc}" style="width:100%; height:500px;"></iframe>
-                <div class="iframe-delete-btn" onclick="closeIframe(this)"></div>
-            `;
-            
-
-            container.appendChild(iframeContainer);
-        }
-        function closeIframe(button) {
-        const iframeContainer = button.parentElement;
-        const noteElement = iframeContainer.parentElement;
-        const openPageBtn = noteElement.querySelector('.open-page-btn');
-        if (openPageBtn) {
-            openPageBtn.style.display = 'inline-block'; // 显示打开网页按钮
-        }
-        iframeContainer.remove();
-    }
-
-
-
-// JavaScript函数用于显示添加新便签的表单
-function showAddNoteForm() {
-    const newTitleElement = document.getElementById('tit-bar');
-    const newTitle = newTitleElement.innerText;
-        if (newTitle.includes('%') || newTitle.includes('#')) {
-            alert('标题包含非法字符(#或%)，删除该字符后再输入。');
-            return;
-          
-        }
-        else if (newTitle == ''){
-            alert('请输入标题');
-            return;
-        }
- 
-
-    // 创建文本区域元素
-    const newContentElement = document.getElementById('word-bar');
-        
-        const newContent = newContentElement.innerText;
-        addNewNoteRequest(newTitle, newContent);
-        newContentElement.innerText ='';
-        newTitleElement.innerText ='';
-}
-
-
-
-
-
-   // 异步函数，用于向服务器发送添加新便签的请求
-async function addNewNoteRequest(newTitle, newContent) {
-    const formData = new FormData();
-    formData.append('newTitle', newTitle);
-    formData.append('newContent', newContent);
-
+// 获取书签
+async function getbok() {
+    const bokbox=document.getElementById('bok-box');
+    bokbox.innerHTML='';
     try {
-        const response = await fetch(`${protocol}//${ip}/code/Bookmark/bok_addNote.php`, {
+        const response = await fetch(`${protocol}//${ip}/code/Bookmark/getbok.php`); 
+        if (!response.ok) {
+           notify("获取失败");
+        }
+        const fileData = await response.json();
+        const fileListElement = document.getElementById('fileList');
+        fileData.forEach(file => {
+            const bok = document.createElement('div');
+            const tit=document.createElement('span');
+            const del = document.createElement('div');
+            titname=file.name.split('.').slice(0, -1).join('.') || file.name;
+            bok.classList.add('bok');
+            tit.classList.add('tit');
+            del.classList.add('del');
+            del.title="删除"+titname;
+            bok.title="打开"+titname;
+            tit.innerText=titname;
+            del.addEventListener('click', function(event) {
+                event.stopPropagation();
+            });
+            bok.addEventListener('click', function() {
+                updatebok(file.name);
+                if (!file.content.startsWith('http')) {
+                    file.content = 'http://' + file.content;
+                }
+                window.location.href = file.content;                
+            });
+            del.addEventListener('click', function() {
+                if (confirm(`确定要删除文件 "${titname}" 吗？`)) {
+                delbok(file.name);
+                }
+            });
+            bok.appendChild(tit);
+            bok.appendChild(del);
+            bokbox.appendChild(bok);
+            pageloading(0);
+        });
+    } catch (error) {
+        console.log(error);
+        notify(error);
+    }
+}
+// 标记最近访问
+async function updatebok(name) {
+    try {
+        const response = await fetch(`${protocol}//${ip}/code/Bookmark/update.php`, {
             method: 'POST',
             headers: {
-                'Authorization': 'Bearer ' + token
+                'Content-Type': 'application/json',
             },
-            body: formData
+            body: JSON.stringify(name), 
         });
-
-        const result = await response.text();
-        console.log(response.status);
-        xmlnologin(response);
-        if (response.status != 401) {
-            document.getElementById('notes').innerHTML = '';
-            notify("添加成功");
-            showNotes();
-        }
     } catch (error) {
-        console.error('Error adding new note:', error);
+        notify(error);
     }
 }
-
-    // JavaScript函数用于删除便签
-    function deleteNote(fileName) {
-        if (confirm('确定要删除这个书签吗？')) {
-            // 发送删除请求
-            deleteNoteFile(fileName);
-            notify("删除成功");
-        }
+// 删除
+function delbok(name){
+    pageloading(1);
+    fetch(`${protocol}//${ip}/code/Bookmark/del.php`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: name }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        notify(data.message)
+       getbok();
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+// 新书签
+function newbok() {
+    pageloading(1);
+    let tit=document.getElementById('tit-bar');
+    let link=document.getElementById('word-bar');
+    let name=tit.value;
+    let text=link.innerText;
+    name=tit.value+".bok";
+    text=link.innerText;
+    if(tit.value==''){
+        name="new_bookmark.bok"
     }
-
-    // 异步函数，用于删除便签文件
-    async function deleteNoteFile(fileName) {
-        const formData = new FormData();
-        formData.append('fileName', fileName);
-
-        try {
-            const response = await fetch(`${protocol}//${ip}/code/Bookmark/bok_deleteNote.php`, {
-                method: 'POST',
-                headers: {
-                    'Authorization': 'Bearer ' + token
-                },
-                body: formData
-            });
-
-            const result = await response.text();
-            console.log(result);
-
-            // 重新加载便签
-            document.getElementById('notes').innerHTML = '';
-            showNotes();
-        } catch (error) {
-            console.error('Error deleting note:', error);
-        }
-    }
-
-  function selfile() {
-  var files = document.getElementsByTagName('input')[0].files;
-  if (files.length === 0) {
-    notify("请先选择文件");
-    return;
-  }
-  document.getElementById('word-bar').style.display='none';
-  document.getElementById('progress').style.display='block';
-  var fd = new FormData();
-  for (var i = 0; i < files.length; i++) {
-    fd.append('pic[]', files[i]);
-  }
-
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', `${protocol}//${ip}/code/Bookmark/upload.php`, true);
-  xmltoken(xhr);
-  xhr.upload.onprogress = function(ev) {
-    if (ev.lengthComputable) {
-      var percent = 100 * ev.loaded / ev.total;
-      var percentElement = document.getElementById('percent');
-      document.getElementById('bar').style.width = percent + '%';
-      percentElement.innerHTML = parseInt(percent) + '%'; // 更新百分比显示
-    }
-  };
-
-  xhr.onload = function() {
-    if (xhr.readyState === XMLHttpRequest.DONE) { // 请求已完成
-        xmlnologin(xhr);
-        if (xhr.status!= 401){
-        notify("添加成功");
-        document.getElementById('notes').innerHTML='';
-        showNotes();
-        document.getElementById('word-bar').style.display='block';
-  document.getElementById('progress').style.display='none';
-        }
-  }
-  };
-
-  xhr.send(fd);
-  
+    tit.value='';
+    link.innerText='';
+    fetch(`${protocol}//${ip}/code/Bookmark/new.php`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: name, text: text })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.message);
+       notify(data.message);
+       getbok();
+    })
+    .catch(error => {
+        notify(error);
+    });
 }
