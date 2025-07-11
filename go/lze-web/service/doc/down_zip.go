6976d93c4fce7c74c48c9428a003d388133706e5
linzeennnn <linzeen2745276404@gmail.com>
2025-07-11 14:31:08 +0800

    finsh react doc

--- go/lze-web/service/doc/down_zip.go
+++ go/lze-web/service/doc/down_zip.go
@@ -24,20 +24,27 @@ func sendFile(path, name string, c *gin.Context) {
 	c.Writer.Header().Set("Content-Disposition",
 		"attachment; filename=\""+name+"\"; filename*=UTF-8''"+encodedName)
 	c.Writer.Header().Set("Content-Type", "application/octet-stream")
+
 	file, err := os.Open(path)
 	if err != nil {
 		c.JSON(500, gin.H{"error": "文件打开失败"})
 		return
 	}
 	defer file.Close()
-	_, err = io.Copy(c.Writer, file)
-	if err != nil {
+
+	// ⚠️ 重点：只在 io.Copy 成功后再删
+	if _, err = io.Copy(c.Writer, file); err != nil {
+		// 插件下载失败或未消费内容
 		c.JSON(500, gin.H{"error": "文件传输失败"})
 		return
 	}
+
 	c.Writer.Flush()
+
+	// 现在可以删了，确保数据已写出
 	go func() {
-		time.Sleep(1 * time.Second)
+		// 可以加一点缓冲时间，防止插件写入缓存
+		time.Sleep(10 * time.Second)
 		os.RemoveAll(filepath.Dir(path))
 	}()
 }
--- react/lze-web/src/widget/doc/content/downloadBtn.jsx
+++ react/lze-web/src/widget/doc/content/downloadBtn.jsx
@@ -1,5 +1,21 @@
+import { useGlobal,ZipDir } from "../global"
+
 export default function DownloadBtn({fileMes}){
+    const global=useGlobal.getState()
+   
     return (
-        <button className="down-btn btn" title={"下载:"+fileMes.name}></button>
+        <button className="down-btn btn" title={"下载:"+fileMes.name}
+        onClick={((e)=>{
+            e.stopPropagation();
+            if(fileMes.type==="file"||fileMes.type==="file_link"){
+                window.location.href = 
+                global.docUrl+"download_file?file_path="+global.nowPath+"/"+fileMes.name+
+                "&token="+global.token+"&user="+global.userName
+            }
+            else{
+                ZipDir(fileMes.name)
+            }
+        })}
+        ></button>
     )
 }
