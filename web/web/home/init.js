8e6b1144defba893a74e47f2267ae413dac11e17
linzeennnn <linzeen2745276404@gmail.com>
2025-08-01 21:40:04 +0800

    change repo struct

--- web/web/home/init.js
+++
@@ -1,8 +0,0 @@
-let doc_path;
-let pic_path;
-let mon_path;
-let not_path;
-let bok_path;
-let tra_path;
-const ishome=1;
-document.getElementById('theme-dis').style.backgroundColor=`var(--default)`;
