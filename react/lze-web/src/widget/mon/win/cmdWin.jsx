f83e966058c2afa7ec2ac6aa69c7ddecc24590e9
linzeennnn <linzeen2745276404@gmail.com>
2025-09-04 23:21:57 +0800

    incomplete

--- react/lze-web/src/widget/mon/win/cmdWin.jsx
+++ react/lze-web/src/widget/mon/win/cmdWin.jsx
@@ -1,29 +1,77 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { WinBg } from "../../../components/winBg"
 import { GetText, useGlobal,loadPage } from "../global"
 import { notify } from "../../../components/notify";
 export default function CmdWin(){
     const showCmd=useGlobal(state=>state.showCmd)
-    const [output,setOutput]=useState("")
+    const [output,setOutput]=useState("输出")
     const [inputCmd,setInputCmd]=useState("")
+    const [cusTitle,setCusTitle]=useState("")
+    const [cusCmd,setCusCmd]=useState("")
+     const [cmdList, setCmdList] = useState([])
+    useEffect(()=>{
+        setCmdList(JSON.parse(localStorage.getItem("cmdList")) || [])
+        
+    },[])
+   const saveCmd = (title, cmd) => {
+    const newList = [...cmdList, { title, cmd }]
+    setCmdList(newList)
+    localStorage.setItem("cmdList", JSON.stringify(newList))
+}
+   const delCmd = (index) => {
+    const newList = [...cmdList, { title, cmd }]
+    setCmdList(newList)
+    localStorage.setItem("cmdList", JSON.stringify(newList))
+}
+    const cusKeyDown=(e)=>{
+            if(e.key==="Enter"){
+            saveCmd(cusTitle,cusCmd)
+        }
+    }
     return(
         <WinBg showBg={showCmd}>
             <div id="cmd-box">
-            <button className="btn" id="close-cmd"
-            title={GetText("close")}
-            onClick={()=>{
-                useGlobal.setState({showCmd:false})
+                <button className="btn" id="close-cmd"
+                title={GetText("close")}
+                onClick={()=>{
+                    useGlobal.setState({showCmd:false})
+                }}
+                ></button>
+            <div id="cus-cmd-box">
+                {cmdList.map((cmd_item,index)=>(
+                        <div className="cmd-item" key={index+cmd_item.cmd}>
+                            <button className="del-cmd btn"></button>
+                            <span className="cmd-title">{cmd_item.title}</span>
+                        </div>
+                ))}
+            </div>
+            <div id="send-box" className="cmd-widget">
+            <input id="input-cmd" className="cmd-input" value={inputCmd}
+            onKeyDown={(e)=>{
+                if(e.key==="Enter"){
+                    sendCmd(inputCmd,setOutput)
+                }
             }}
-            ></button>
-            <input id="input-cmd" value={inputCmd}
             onChange={(e)=>setInputCmd(e.target.value)}
             />
+            <pre id="output-text">{output}</pre>
             <button className="btn" id="send-cmd"
             onClick={()=>{
                 sendCmd(inputCmd,setOutput)
             }}
             ></button>
-            <pre id="output-text">{output}</pre>
+            </div>
+            <div id="cus-cmd-input-bar"  className="cmd-widget">
+                <input placeholder="自定义名称"  className="cmd-input" onKeyDown={cusKeyDown}
+                value={cusTitle} onChange={(e)=>setCusTitle(e.target.value)}/>
+                <input placeholder="命令"  className="cmd-input"  onKeyDown={cusKeyDown}
+                value={cusCmd} onChange={(e)=>setCusCmd(e.target.value)}/>
+                <button className="btn" id="add-cmd"
+                onClick={()=>{
+                    saveCmd(cusTitle,cusCmd)
+                }}
+                ></button>
+            </div>
             </div>
         </WinBg>
     )
