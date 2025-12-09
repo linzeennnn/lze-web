5a92a0c2376546d07b45fe8f627154bd61eec495
zane <zane@zane.local>
2025-12-09 20:22:15 +0800

    fix file win layout

--- react/lze-web/src/widget/doc/win/fileWin.jsx
+++ react/lze-web/src/widget/doc/win/fileWin.jsx
@@ -29,11 +29,6 @@ export default function FileWin(){
         const style = doc.createElement("style")
         style.textContent = `
             body {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                margin: 0;
-                padding: 0;
                 width: 100%;
                 height: 100%;
             }
